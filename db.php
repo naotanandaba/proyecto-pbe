c5e97baf7fa46f6c5c4760dc79d1639a4a4fe3f5
Xavi <xguinotv@gmail.com>
2021-04-25 12:43:46 +0200

    beta working

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
 $conn = mysqli_connect(
-    'localhost',
-    'pbe_user',
-    'pbepass',
-    'pbe_table',
+    'sql11.freesqldatabase.com:3306',
+    'sql11407041',
+    '2q8mGNLwAE',
+    'sql11407041',
 );
 
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,65 @@
 <?php
-include ("db.php");
-$query = "SELECT * FROM timetables";
-echo $_GET['limit'];
-$query .= " LIMIT " . $_GET['limit'];
+include_once("db.php");
+function where_or_and($q, $s){
+  $buffer = $s ? " AND" : " WHERE";
+  $q .= $buffer;
+  return $q;
+}
+parse_str($_SERVER['QUERY_STRING'], $output);
+$start = False;
+$query = "SELECT * FROM marks";
+
+if(isset($output['subject'])){
+  $query .= " WHERE subject=" . "'" . $output['subject'] . "'";
+  $start = True;
+}
+/*if(isset($name)){
+
+  $query = where_or_and($query,$start);
+
+  $query .= " name=" . "'" . $name . "'";
+  $start=True;
+}
+if(isset($mark[lt])){
+  $query = where_or_and($query, $start);
+  $query .= " mark <=" . $mark[lt];
+  $start = True;
+}
+if(isset($mark[eq])){
+   $query = where_or_and($query, $start);
+   $query .= " mark =" . $mark[eq];
+   $start = True;
+ }
+if(isset($mark[gt])){
+   $query = where_or_and($query, $start);
+   $query .= " mark >=" . $mark[gt];
+   $start = True;
+ }
+*/
+
+
+
+
+
+
+if(isset($limit)){
+  $query .= " LIMIT " . $limit;
+}
 $result = mysqli_query($conn, $query);
 if(!$result){
     die("query failed");
-
 }
-$data= array();
 
-while ($row = mysqli_fetch_array($result)){
+
+
+
+
+while ($row = mysqli_fetch_assoc($result)){
     $data[]=$row;
 }
-echo json_encode($data, );
+
+echo json_encode($data);
+
+
 
 ?>
