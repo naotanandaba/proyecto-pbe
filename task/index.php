3e696cfa4c4a65b69cf1a55485cb1c7f69415bcc
Xavi <xavierguinot1@gmail.com>
2021-04-27 11:36:13 +0200

    end

--- task/index.php
+++ task/index.php
@@ -9,34 +9,37 @@ parse_str($_SERVER['QUERY_STRING'], $constraints);
 $start = False;
 $query = "SELECT * FROM task";
 
+
+
 if(isset($constraints['subject'])){
-  $query = where_or_and($query,$start);
+  $query = where_or_and($query, $start);
   $query .= " subject=" . "'" . $constraints['subject'] . "'";
   $start = True;
 }
 
 if(isset($constraints['date']['gt'])){
-  $query = where_or_and($query,$start);
+  $query = where_or_and($query, $start);
   $query .= "  date>=" . "'" . $constraints['date']['gt'] . "'";
   $start = True;
 }
 
 if(isset($constraints['date']['eq'])){
-  $query = where_or_and($query,$start);
+  $query = where_or_and($query, $start);
   $query .= " date=" . "'" . $constraints['date']['eq'] . "'";
   $start = True;
 }
 
 if(isset($constraints['date']['lt'])){
-  $query = where_or_and($query,$start);
+  $query = where_or_and($query, $start);
   $query .= " date<=" . "'" . $constraints['date']['lt'] . "'";
   $start = True;
 }
 
-
 if(isset($constraints['limit'])){
   $query .= " LIMIT " . $constraints['limit'];
 }
+
+
 
 $result = mysqli_query($conn, $query);
 if(!$result){
